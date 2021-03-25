212723ba618101fe6aa63b410c289e01a2c65eb6
Lian <lianshanchun@baidu.com>
2021-03-26 02:47:12 +0800

    add micro-template

---
+++ main-app/config/routes.ts
@@ -0,0 +1,58 @@
+﻿export default [
+    {
+        path: '/user',
+        layout: false,
+        routes: [
+            {
+                path: '/user',
+                routes: [
+                    {
+                        name: 'login',
+                        path: '/user/login',
+                        component: './user/Login',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        name: 'sub-app-1',
+        icon: 'smile',
+        path: '/sub-app-1',
+        microApp: 'sub-app-1',
+    },
+    {
+        path: '/welcome',
+        name: 'welcome',
+        icon: 'smile',
+        component: './Welcome',
+    },
+    {
+        path: '/admin',
+        name: 'admin',
+        icon: 'crown',
+        access: 'canAdmin',
+        component: './Admin',
+        routes: [
+            {
+                path: '/admin/sub-page',
+                name: 'sub-page',
+                icon: 'smile',
+                component: './Welcome',
+            },
+        ],
+    },
+    {
+        name: 'list.table-list',
+        icon: 'table',
+        path: '/list',
+        component: './TableList',
+    },
+    {
+        path: '/',
+        redirect: '/welcome',
+    },
+    {
+        component: './404',
+    },
+];
